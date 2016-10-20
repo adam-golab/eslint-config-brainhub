c064b8a3b3736c881729da6e3e7cd1e7db4bd0c6
Adam <adam@brainhub.pl>
2016-10-20 15:09:05 +0200

    remove comments

--- index.js
+++ index.js
@@ -1,19 +1,3 @@
-/**
- * Copyright 2016 Google Inc. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 'use strict';
 
 module.exports = {
@@ -39,167 +23,49 @@ module.exports = {
     "import"
   ],
   rules: {
-    // The rules below are listed in the order they appear on the eslint
-    // rules page. All rules are listed to make it easier to keep in sync
-    // as new ESLint rules are added.
-    // http://eslint.org/docs/rules/
-    // - Rules in the `eslint:recommended` ruleset that aren't specifically
-    //   mentioned by the google styleguide are listed but commented out (so
-    //   they don't override a base ruleset).
-    // - Rules that are recommended but contradict the Google styleguide
-    //   are explicitely set to the Google styleguide value.
-
-    // Possible Errors
-    // http://eslint.org/docs/rules/#possible-errors
-    // ---------------------------------------------
-    'no-cond-assign': 2, // eslint:recommended
-    'no-console': 1, // eslint:recommended 2
-    // 'no-condition': 2, // eslint:recommended
-    // 'no-constant-condition': 2, // eslint:recommended
-    // 'no-control-regex': 2, // eslint:recommended
-    'no-debugger': 1, // eslint:recommended 2
-    // 'no-dupe-args': 2, // eslint:recommended
-    // 'no-dupe-keys': 2, // eslint:recommended
-    // 'no-duplicate-case': 2, // eslint:recommended
-    // 'no-empty-character-class': 2, // eslint:recommended
-    // 'no-empty': 2, // eslint:recommended
-    // 'no-ex-assign': 2, // eslint:recommended
-    // 'no-extra-boolean-cast': 2, // eslint:recommended
-    // 'no-extra-parens': 0,
-    // 'no-extra-semi': 2, // eslint:recommended
-    // 'no-func-assign': 2, // eslint:recommended
-    // 'no-inner-declarations': 2, // eslint:recommended
-    // 'no-invalid-regexp': 2, // eslint:recommended
-    'no-irregular-whitespace': 2, // eslint:recommended
-    // 'no-obj-calls': 2, // eslint:recommended
-    // 'no-prototype-builtins': 0,
-    // 'no-regex-spaces': 2, // eslint:recommended
-    // 'no-sparse-arrays': 2, // eslint:recommended
-    // 'no-template-curly-in-string': 0,
-    'no-unexpected-multiline': 2, // eslint:recommended
-    // 'no-unreachable': 2, // eslint:recommended
-    // 'no-unsafe-finally': 2, // eslint:recommended
+    'no-cond-assign': 2,
+    'no-console': 1,
+    'no-debugger': 1,
+    'no-irregular-whitespace': 2,
+    'no-unexpected-multiline': 2,
     'no-unsafe-negation': 2,
-    // 'use-isnan': 2 // eslint:recommended
     'valid-jsdoc': [2, {
       requireParamDescription: false,
       requireReturn: false,
       prefer: {returns: 'return'},
     }],
-    // 'valid-typeof': 2 // eslint:recommended
-
-
-    // Best Practices
-    // http://eslint.org/docs/rules/#best-practices
-    // --------------------------------------------
-
-    // 'accessor-pairs': 0,
-    // 'array-callback-return': 0,
-    // 'block-scoped-var': 0,
-    // 'class-methods-use-this': 0,
-    // 'complexity': 0,
-    // 'consistent-return': 0
     'curly': 2,
     'default-case': 2,
-    // 'dot-location': 0,
     'dot-notation': 2,
     'eqeqeq': 2,
     'guard-for-in': 2,
     'no-alert': 1,
     'no-caller': 2,
-    // 'no-case-declarations': 2, // eslint:recommended
-    // 'no-div-regex': 0,
-    // 'no-else-return': 0,
     'no-empty-function': 2,
-    // 'no-empty-pattern': 2, // eslint:recommended
-    // 'no-eq-null': 0,
-    // 'no-eval': 0,
     'no-extend-native': 2,
     'no-extra-bind': 2,
     'no-extra-label': 2,
-    // 'no-fallthrough': 2, // eslint:recommended
-    // 'no-floating-decimal': 0,
     'no-global-assign': 2,
-    // 'no-implicit-coercion': 0,
-    // 'no-implicit-globals': 0,
-    // 'no-implied-eval': 0,
     'no-invalid-this': 2,
-    // 'no-iterator': 0,
     'no-labels': 2,
-    // 'no-lone-blocks': 0,
     'no-loop-func': 2,
-    // 'no-magic-numbers': 0,
     'no-multi-spaces': 2,
     'no-multi-str': 2,
     'no-new-func': 2,
     'no-new-wrappers': 2,
     'no-new': 2,
-    // 'no-octal-escape': 0,
-    // 'no-octal': 2, // eslint:recommended
-    // 'no-param-reassign': 0,
-    // 'no-proto': 0,
-    // 'no-redeclare': 2, // eslint:recommended
-    // 'no-return-assign': 0,
-    // 'no-script-url': 0,
-    // 'no-self-assign': 2, // eslint:recommended
-    // 'no-self-compare': 0,
-    // 'no-sequences': 0,
-    'no-throw-literal': 2, // eslint:recommended
+    'no-throw-literal': 2,
     'no-unmodified-loop-condition': 2,
-    // 'no-unused-expressions': 2,
-    // 'no-unused-labels': 2, // eslint:recommended
-    // 'no-useless-call': 0,
-    // 'no-useless-concat': 0,
-    // 'no-useless-escape': 0,
     'no-void': 2,
     'no-warning-comments': 2,
     'no-with': 2,
     'radix': [2, "as-needed"],
-    // 'vars-on-top': 0,
-    // 'wrap-iife': 0,
-    // 'yoda': 0,
-
-    // Strict Mode
-    // http://eslint.org/docs/rules/#strict-mode
-    // -----------------------------------------
-    // 'script': 0,
-
-    // Variables
-    // http://eslint.org/docs/rules/#variables
-    // ---------------------------------------
-    // 'init-declarations': 0,
-    // 'no-catch-shadow': 0,
-    // 'no-delete-var': 2, // eslint:recommended
-    // 'no-label-var': 0,
-    // 'no-restricted-globals': 0,
-    // 'no-shadow-restricted-names': 0,
     'no-shadow': 0,
-    // 'no-undef-init': 0,
-    'no-undef': 2, // eslint:recommended
+    'no-undef': 2,
     'no-undefined': 2,
-    'no-unused-vars': [2, {args: 'none'}], // eslint:recommended
+    'no-unused-vars': [2, {args: 'none'}],
     'no-use-before-define': 2,
-
-    // Node.js and CommonJS
-    // http://eslint.org/docs/rules/#nodejs-and-commonjs
-    // -------------------------------------------------
-    // 'callback-return': 0,
-    // 'global-require': 0,
-    // 'handle-callback-err': 0,
-    // 'no-mixed-requires': 0,
-    // 'no-new-require': 0,
-    // 'no-path-concat': 0,
-    // 'no-process-env': 0,
-    // 'no-process-exit': 0,
-    // 'no-restricted-modules': 0,
-    // 'no-restricted-properties': 0,
-    // 'no-sync': 0,
-
-    // Stylistic Issues
-    // http://eslint.org/docs/rules/#stylistic-issues
-    // ----------------------------------------------
     'array-bracket-spacing': [2, 'never'],
-    // 'block-spacing': 0,
     'brace-style': 2,
     'camelcase': 2,
     'comma-dangle': [2, 'always-multiline'],
@@ -209,21 +75,10 @@ module.exports = {
     'consistent-this': 2,
     'eol-last': 2,
     'func-call-spacing': 2,
-    // 'func-names': 0,
-    // 'func-style': 0,
-    // 'id-blacklist': 0,
-    // 'id-length': 0,
-    // 'id-match': 0,
-    // 'indent': 0, // TODO(philipwalton): this rule isn't compatible with
-                    // Google's 4-space indent for line continuations.
     'jsx-quotes': [2, 'prefer-double'],
     'key-spacing': 2,
     'keyword-spacing': 2,
-    // 'line-comment-position': 0,
     'linebreak-style': 0,
-    // 'lines-around-comment': 0,
-    // 'lines-around-directive': 0,
-    // 'max-depth': 0,
     'max-len': [2, {
       code: 120,
       tabWidth: 2,
@@ -232,98 +87,41 @@ module.exports = {
       ignoreStrings: true,
       ignoreTemplateLiterals: true
     }],
-    // 'max-lines': 0,
-    // 'max-nested-callbacks': 0,
-    // 'max-params': 0,
-    // 'max-statements-per-line': 0,
-    // 'max-statements': 0,
-    // 'multiline-ternary': 0, // TODO(philipwalton): add a rule to enfore the
-                               // operator appearing at the end of the line.
     'new-cap': 2,
     'new-parens': 2,
-    // 'newline-after-var': 0,
-    // 'newline-before-return': 0,
-    // 'newline-per-chained-call': 0,
     'no-array-constructor': 2,
-    // 'no-bitwise': 0,
-    // 'no-continue': 0,
-    // 'no-inline-comments': 0,
     'no-lonely-if': 2,
-    // 'no-mixed-operators': 0,
-    'no-mixed-spaces-and-tabs': 2, // eslint:recommended
+    'no-mixed-spaces-and-tabs': 2,
     'no-multiple-empty-lines': [2, {max: 2}],
-    // 'no-negated-condition': 0,
-    // 'no-nested-ternary': 0,
     'no-new-object': 2,
-    // 'no-plusplus': 0,
-    // 'no-restricted-syntax': 0,
-    // 'no-tabs': 0,
-    // 'no-ternary': 0,
     'no-trailing-spaces': 2,
-    // 'no-underscore-dangle': 0,
-    // 'no-unneeded-ternary': 0,
     'no-whitespace-before-property': 2,
-    // 'object-curly-newline': 0,
     'object-curly-spacing': [2, 'always'],
-    // 'object-property-newline': 0,
-    // 'one-var-declaration-per-line': 0,
     'one-var': [2, {
       var: 'never',
       let: 'never',
       const: 'never',
     }],
-    // 'operator-assignment': 0,
-    // 'operator-linebreak': 0,
     'padded-blocks': [2, 'never'],
     'quote-props': [2, 'as-needed'],
     'quotes': [2, 'single', {allowTemplateLiterals: true}],
     'require-jsdoc': 0,
     'semi-spacing': 2,
     'semi': 2,
-    // 'sort-keys': 0,
-    // 'sort-vars': 0,
     'space-before-blocks': 2,
     'space-before-function-paren': [2, 'never'],
-    // 'space-in-parens': 0,
-    // 'space-infix-ops': 0,
-    // 'space-unary-ops': 0,
     'spaced-comment': [2, 'always'],
-    // 'unicode-bom': 0,
-    // 'wrap-regex': 0,
-
-    // ECMAScript 6
-    // http://eslint.org/docs/rules/#ecmascript-6
-    // ------------------------------------------
-    // 'arrow-body-style': 0,
     'arrow-parens': [2, 'as-needed'],
     'arrow-spacing': 2,
-    'constructor-super': 2, // eslint:recommended
+    'constructor-super': 2,
     'generator-star-spacing': [2, 'before'],
-    // 'no-class-assign': 0,
-    // 'no-confusing-arrow': 0,
-    // 'no-const-assign': 0, // eslint:recommended
-    // 'no-dupe-class-members': 0, // eslint:recommended
-    // 'no-duplicate-imports': 0,
-    'no-new-symbol': 2, // eslint:recommended
-    // 'no-restricted-imports': 0,
-    'no-this-before-super': 2,  // eslint:recommended
-    // 'no-useless-computed-key': 0,
-    // 'no-useless-constructor': 0,
-    // 'no-useless-rename': 0,
+    'no-new-symbol': 2,
+    'no-this-before-super': 2,
     'no-var': 2,
-    // 'object-shorthand': 0,
-    // 'prefer-arrow-callback': 0,
     'prefer-const': 2,
-    // 'prefer-numeric-literals': 0,
-    // 'prefer-reflect': 0,
     'prefer-rest-params': 2,
     'prefer-spread': 2,
-    // 'prefer-template': 0,
-    // 'require-yield': 2, // eslint:recommended
     'rest-spread-spacing': 2,
-    // 'sort-imports': 0,
-    // 'symbol-description': 0,
-    // 'template-curly-spacing': 0,
     'yield-star-spacing': [2, 'before'],
   },
 };
