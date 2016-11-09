a9b85b0b5adbd2d04d4028c6d93b5c2c730849a4
Adam <adam@brainhub.pl>
2016-11-09 16:04:20 +0100

    added rules for operator-linebreak

--- index.js
+++ index.js
@@ -104,6 +104,7 @@ module.exports = {
       let: 'never',
       const: 'never',
     }],
+    'operator-linebreak': [2, 'after', { 'overrides': { '?': 'before', ':': 'before' } }],
     'padded-blocks': [2, 'never'],
     'quote-props': [2, 'as-needed'],
     'quotes': [2, 'single', {allowTemplateLiterals: true}],
